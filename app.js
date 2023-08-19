5103f9c0dea24c4e58531f330e491dad3f431e24
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2023-08-19 22:44:59 +0600

    SwaggerUI for API Documentation

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -39,8 +44,11 @@ app.use(xss());
 //extra packages
 
 app.get('/', (req, res) => {
-    res.send('<h1>Jobs API</h1>');
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+// Swagger
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
