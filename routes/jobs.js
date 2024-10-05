b22f54d897b1c5ddc5f9c4233bc9c3e53bed2f7f
tanvir1418 <tanvirahmed1418@gmail.com>
2024-10-05 13:54:02 +0600

    Code formatted

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const {
-    getAllJobs,
-    getJob,
-    createJob,
-    updateJob,
-    deleteJob,
-} = require('../controller/job');
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+} = require("../controller/job");
 
-router.route('/').post(createJob).get(getAllJobs);
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob);
+router.route("/").post(createJob).get(getAllJobs);
+router.route("/:id").get(getJob).delete(deleteJob).patch(updateJob);
 
 module.exports = router;
